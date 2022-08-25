5a1601fa38b2aeac198f05cd4b49a42750872cc2
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2022-08-25 16:38:43 +0200

    added some more loops

--- static/FizzBuzzAppForLoop.js
+++ static/FizzBuzzAppForLoop.js
@@ -1,3 +1,5 @@
+// for Loop
+
 for (let b = 1; b < 101; b++) {
     if (b % 15 === 0){
         console.log("FizzBuzz");
@@ -12,3 +14,46 @@ for (let b = 1; b < 101; b++) {
         console.log(b)
     }
 }
+
+// while loop
+
+let c = 0;
+
+while (c < 100) {
+    c++;
+
+    if(c % 15 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (c % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (c % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(c)
+    }
+}
+
+// do while loop
+
+
+let d = 1;
+
+do {
+    if (d % 15 === 0){
+        console.log("FizzBuzz")
+    }
+    else if (d % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (d % 5 === 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(d);
+    }
+    d++;
+}
+while (d < 101)
